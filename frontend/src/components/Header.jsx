c6316999a903a1638476c46a18b9b8136eee0faa
AnandBhalla <bhallaanand4@gmail.com>
2025-07-08 20:35:58 +0530

    added hamburger

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import { IoMdPerson } from "react-icons/io";
-import logo from '../assets/logo.png'
-import './Header.css';
+import { FiMenu } from "react-icons/fi"; // hamburger icon
+import logo from "../assets/logo.png";
+import "./Header.css";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="container">
         <div className="logo">
           <img src={logo} id="logo" />
         </div>
-        <nav className="nav">
-          <a href="#" className="nav-link"><IoMdPerson id="person" /> Become an Interviewer ↗</a>
-          <a href="#" className="nav-link">features</a>
+
+        {/* Hamburger icon for mobile */}
+        <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          <FiMenu />
+        </button>
+
+        {/* Navigation */}
+        <nav className={`nav ${menuOpen ? "nav-open" : ""}`}>
+          <a href="#" className="nav-link">
+            <IoMdPerson id="person" /> Become an Interviewer ↗
+          </a>
+          <a href="#" className="nav-link">Features</a>
           <a href="#" className="nav-link">Pricing</a>
           <a href="#" className="nav-link">Start a trial</a>
           <button className="login-btn">Login</button>
